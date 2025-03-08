6c2edd7a298c99d45c7dd0f0d7389f8d92138285
João Pedro Henrique <joao.pedro.hsd@gmail.com>
2025-03-08 00:55:16 -0300

    feat: add data source for fetching organization info (#60)

---
+++ internal/provider/organization_data_source.go
@@ -0,0 +1,106 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+	"terraform-provider-turso/internal/client"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+var _ datasource.DataSource = &OrganizationDataSource{}
+
+func NewOrganizationDataSource() datasource.DataSource {
+	return &OrganizationDataSource{}
+}
+
+// OrganizationDataSource defines the data source implementation.
+type OrganizationDataSource struct {
+	client *client.Client
+}
+
+// OrganizationDataSourceModel describes the data source data model.
+type OrganizationDataSourceModel struct {
+	Slug types.String `tfsdk:"slug"`
+
+	// Computed
+	Name types.String `tfsdk:"name"`
+	Type types.String `tfsdk:"type"`
+}
+
+func (d *OrganizationDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_organization"
+}
+
+func (d *OrganizationDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: "Organization data source",
+
+		Attributes: map[string]schema.Attribute{
+			"slug": schema.StringAttribute{
+				MarkdownDescription: "The organization slug. This will be your username for `personal` accounts.",
+				Required:            true,
+			},
+			"name": schema.StringAttribute{
+				MarkdownDescription: "The organization name. Every user has a `personal` organization for their own account.",
+				Computed:            true,
+			},
+			"type": schema.StringAttribute{
+				MarkdownDescription: "The type of account this organization is. Will always be `personal` or `team`.",
+				Computed:            true,
+			},
+		},
+	}
+}
+
+func (d *OrganizationDataSource) Configure(ctx context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*client.Client)
+
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Data Source Configure Type",
+			fmt.Sprintf("Expected *http.Client, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+
+	d.client = client
+}
+
+func (d *OrganizationDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var data OrganizationDataSourceModel
+
+	// Read Terraform configuration data into the model
+	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	res, err := d.client.GetOrganization(ctx, client.GetOrganizationParams{
+		OrganizationSlug: data.Slug.ValueString(),
+	})
+
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to read organization, got error: %s", err.Error()))
+		return
+	}
+
+	switch p := res.(type) {
+	case *client.GetOrganizationOK:
+		data.Name = types.StringValue(p.Organization.Value.Name.Value)
+		data.Type = types.StringValue(string(p.Organization.Value.Type.Value))
+		data.Slug = types.StringValue(p.Organization.Value.Slug.Value)
+	}
+
+	// Save data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
